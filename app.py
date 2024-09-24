0e80867b5a8494627c130e4c9977674896e0a60d
vishalsofttech <vishalbfcsofttech@gmail.com>
2024-09-24 13:36:56 +0530

    submit form

--- app.py
+++ app.py
@@ -1,6 +1,19 @@
-from flask import Flask, render_template,request
+from flask import Flask, render_template,request,flash
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config["SECRET_KEY"] = "We are legends.@#$%"
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///data.db"
+db = SQLAlchemy(app)
+
+class User(db.Model):
+    id = db.Column(db.Integer,primary_key=True)
+    first_name = db.Column(db.String(80))
+    last_name = db.Column(db.String(80))
+    email = db.Column(db.String(80))
+    date = db.Column(db.Date)
+    occupation = db.Column(db.String(80))
 
 @app.route("/",methods=["GET","POST"])
 def index():
@@ -8,14 +21,17 @@ def index():
         first_name = request.form["first_name"]
         last_name = request.form["last_name"]
         email = request.form["email"]
-        date = request.form["date"]
+        date = datetime.strptime(request.form["date"],"%Y-%m-%d")
         occupation = request.form["occupation"]
-        print(first_name)
-        print(last_name)
-        print(email)
-        print(date)
-        print(occupation)
+
+        user = User(first_name=first_name,last_name=last_name,email=email,date=date,occupation=occupation)
+        db.session.add(user)
+        db.session.commit()
+        flash(f"{first_name}, Your form is submitted successfully.","success")
 
     return render_template("index.html")
 
-app.run(debug=True,port=4000)
+if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True,port=4000)
